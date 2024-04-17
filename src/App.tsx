09b6f8815a05447a0fd26232f377d01dc3342cd1
DKS0712593 <saurabh.madhure@dcsg.com>
2024-04-17 16:39:32 +0530

    changed the icon and added some configuration in tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./image.jpg";
 import "./App.css";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://github.com/Saleemfero1/DevPortfolioGallery"
+          href="https://github.com/Saleemfero1/DevPortfolioGallery/tree/dev"
           target="_blank"
           rel="noopener noreferrer">
           PORTFOLIO
