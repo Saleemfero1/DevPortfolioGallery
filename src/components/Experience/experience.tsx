3356f32a1ad575e5efa2543eaa32f34ca33f8706
Saleemfero1 <saleemfero8143@gmail.com>
2024-04-29 20:54:32 +0530

    added props to experience card

--- src/components/Experience/experience.tsx
+++ src/components/Experience/experience.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Experience.module.css";
 import ExperienceCard from "./experienceCard";
+import { experience } from "src/data/experience";
 const Experience = () => {
   return (
     <section id="experience" className={styles.experience}>
@@ -8,8 +9,17 @@ const Experience = () => {
         <h3>Experience</h3>
       </div>
       <div className={styles.experienceCardContainer}>
-        <ExperienceCard />
-        <ExperienceCard />
+        {experience.map((experience) => {
+          return (
+            <ExperienceCard
+              startDate={experience.startDate}
+              endDate={experience.endDate}
+              role={experience.endDate}
+              description={experience.desc}
+              company={experience.company}
+            />
+          );
+        })}
       </div>
     </section>
   );
--- src/components/Experience/experienceCard.tsx
+++ src/components/Experience/experienceCard.tsx
@@ -5,33 +5,30 @@ interface ExperienceCard {
   endDate: string;
   role: String;
   company: string;
-  location: string;
   description: string;
 }
-const ExperienceCard = () => {
+const ExperienceCard = ({
+  startDate,
+  endDate,
+  role,
+  company,
+  description,
+}: ExperienceCard) => {
   return (
     <div className={styles.experienceCard}>
       <div className={styles.duration}>
-        <div>Mar-2023</div>
-        <div>August-2023</div>
+        <div>{startDate}</div>
+        <div>{endDate}</div>
       </div>
       <div className={styles.divider}>
         <div className={styles.circle}></div>
         <div className={styles.line}></div>
       </div>
       <div className="experienceDesc">
-        <div className={styles.role}>Associate software Engineer intern</div>
-        <div className={styles.company}>Nextuple, Banglore</div>
-        <div className={styles.description}>
-          -Enhanced data upload process using CSV files, optimizing data parsing
-          and validation for improved accuracy and speed. <br />
-          -Applied reactive programming techniques to convert blocking code
-          segments into non-blocking, enhancing application responsiveness.
-          <br />
-          -Collaborated on frontend development and resolved frontend bugs,
-          showcasing strong problem-solving skills for a more polished and
-          functional application.
-        </div>
+        <div className={styles.role}>{role}</div>
+        <div className={styles.company}>{company}</div>
+        <div className={styles.description}>{description}</div>
+        <div />
       </div>
     </div>
   );
