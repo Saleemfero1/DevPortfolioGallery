57fcf2dcd4865932c91b4aee15c519a13ddd11ba
saleemfero1 <saleemfero8143@gmail.com>
2024-06-19 13:19:10 +0530

    Added mobile navbar

---
+++ src/components/scrollTop/ScrollTop.tsx
@@ -0,0 +1,15 @@
+import ScrollToTop from "react-scroll-to-top";
+
+const ScrollTop = () => {
+  return (
+    <ScrollToTop
+      smooth
+      style={{
+        backgroundColor: "#3dcfb6cc",
+        boxShadow: "inset -4px 4px 8px black, inset 4px -4px 8px black",
+      }}
+    />
+  );
+};
+
+export default ScrollTop;
